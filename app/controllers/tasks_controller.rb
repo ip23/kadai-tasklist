012b3f274d394d25fda17e8870455a2de30e961f
ip23 <cline_cvm@yahoo.co.jp>
2017-08-09 12:31:00 +0000

    minor modification

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -54,7 +54,7 @@ class TasksController < ApplicationController
   
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:content, :task)
+    params.require(:task).permit(:content, :status)
   end
   
 end
